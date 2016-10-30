960d2130f456f6a7bf77f30e4e65afc14dd80c46
David <davidroman96@gmail.com>
2016-10-30 23:47:25 +0000

    recurse directory & add option to skip void files

--- main.go
+++ main.go
@@ -1,8 +1,37 @@
 package main
 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
 type Options struct {
 	skipVoid bool
 }
 
+var opt Options
+
+func visit(path string, f os.FileInfo, err error) error {
+	if err != nil {
+		panic(err)
+	}
+
+	if opt.skipVoid && f.Size() == 0 {
+		return nil
+	}
+
+	fmt.Printf("Visited: %s\n", path)
+	return nil
+}
+
 func main() {
+	opt = Options{}
+	//TODO: Get options from command line flags
+
+	err := filepath.Walk(GetUserHome(), visit)
+
+	if err != nil {
+		panic(err)
+	}
 }
